d3dd2f064fb7072231282b56ebcad1c6749e8a70
Vaclav Horacek <vhoracek@c3r4s1.42prague.com>
2025-03-28 23:31:45 +0100

    rewrite convert  and look for segfault issue

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vhoracek <vhoracek@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 11:30:50 by vhoracek          #+#    #+#             */
-/*   Updated: 2025/03/26 00:55:34 by vhoracek         ###   ########.fr       */
+/*   Updated: 2025/03/28 23:30:54 by vhoracek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ int	main()
 	
 	str = "This is number";
 	num = 12345678;
-	chars = ft_printf("Test %i string: %s", num, str);
-	printf("CHARS PRinTED: %i STRING: %s", chars, str);
+	chars = ft_printf("Test %p string: %s\n", &num, str);
+	printf("CHARS PRinTED: %i STRING: %s\n", chars, str);
 	return (0);
 }
 
@@ -44,10 +44,13 @@ int	ft_printf(const char *str, ...)
 	while (str)
 	{
 		if (*str == '%')
+		{
 			len += convert(*++str, ap);
+			str++;
+		}
 		else
 			len += write(1, (const void*)str++, 1);
-		++len;
+//		++len;
 	}
 	va_end(ap);
 	return (len);
@@ -67,7 +70,9 @@ int	convert(char c_char, va_list ap)
 	else if (c_char == 's')
 	{
 		out = ft_strdup(va_arg(ap, char *));
-		return (write(1, out, ft_strlen(out)));
+		len = write(1, out, ft_strlen(out));
+		free(out);
+		return (len);
 	}
 	else if (c_char == 'd' || c_char == 'i' || c_char == 'u')
 	{
@@ -90,7 +95,9 @@ int	convert(char c_char, va_list ap)
 			out = ft_strupcase(ft_bitoa(va_arg(ap, long long int), 16));
 		else
 			out = ft_bitoa(va_arg(ap, long long int), 16);
-		return (len + write(1, out, ft_strlen(out)));
+		len += write(1, out, ft_strlen(out));
+		free(out);
+		return (len);
 	}
 	else if (c_char == '%')
 		return (write(1, &c_char, 1));
@@ -99,4 +106,68 @@ int	convert(char c_char, va_list ap)
 		write(1, &"%", 1);
 		return (write(1, &c_char, 1) + 1);
 	}
+}
+
+int convert(char c_char, va_list ap)
+{
+	char	*out;
+	int		len;
+	void	*arg;
+
+	len = 0;
+	arg = va_arg(ap, void*);
+	
+	// get value
+	if (c_char == 'c')
+	{
+		if(out = malloc(sizeof(char)) == NULL)
+			return (37707);
+		*out = (char)&arg;
+	}
+	else if (c_char == 's')
+	{
+/*		len = ft_strlen((const char*)arg);
+		if(out = malloc(len * sizeof(char)) == NULL)
+			return (37707);*/
+		out = ft_strdup((const char*)arg);
+//		len = 0;
+	}
+	else if (c_char == 'd' || c_char == 'i' || c_char == 'u')
+	{
+		if (c_char == 'u')
+			out = ft_utoa((unsigned int)arg);
+		else
+			out = ft_itoa((int)arg);
+	}
+	else if (c_char == 'p' || c_char == 'x' || c_char == 'X')
+	{
+		if (c_char == 'p')
+		{
+			len = write(1, "0x", 2);
+			out = ft_bitoa((long long int)arg, 16);
+		}
+		else if (c_char == 'X')
+			out = ft_strupcase(ft_bitoa((long long int)arg, 16)); /// can I now really free out? isnt it linked too far> out > strupcase > bitoa(malloc here)
+		else
+			out = ft_bitoa((long long int)arg, 16);
+	}
+/// unfinished
+	else if (c_char == '%')
+		return (write(1, &c_char, 1));
+	else
+	{
+		write(1, &"%", 1);
+		return (write(1, &c_char, 1) + 1);
+	}
+/// till here
+	if (out == NULL)
+		return (37707);
+	len += write(1, out, ft_strlen(out));
+	free(out);
+	return (len);
+	// malloc
+	// strlen
+	// print out
+	// free malloc
+	// return len
 }
