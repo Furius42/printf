0c58464c2d0ffeccb7f1aa13482f75d6aa379e64
Vaclav Horacek <vhoracek@c3r4s1.42prague.com>
2025-04-22 20:53:04 +0200

    normed main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vhoracek <vhoracek@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 14:24:28 by vhoracek          #+#    #+#             */
-/*   Updated: 2025/04/22 20:40:34 by vhoracek         ###   ########.fr       */
+/*   Updated: 2025/04/22 20:51:06 by vhoracek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,19 @@ int	convert(char c_char, va_list ap);
 
 int	main(int argc, char *argv[])
 {
-	if(argc != 3)
-		return(ft_printf("Give 2 arguments: 1/int 2/str\n"));
-	
 	char	*str;
 	int		num;
 	int		chars;
 
+	if (argc != 3)
+		return (ft_printf("Give 2 arguments: 1/int 2/str\n"));
 	num = ft_atoi(argv[1]);
 	str = argv[2];
 	chars = ft_printf("U: %u\nI: %i\nD: %d\nS: %s\nC: %c\n%%\np: %p\nx: %x\nX %X\n", num, num, num, str, str[0], &num, num, num);
 	ft_printf("CHARS Printed: %i\n\n", chars);
 	chars = printf("U: %u\nI: %i\nD: %d\nS: %s\nC: %c\n%%\np: %p\nx: %x\nX %X\n", num, num, num, str, str[0], &num, num, num);
 	printf("CHARS PrintF: %i\n\n", chars);
-	ft_printf("Test: %p, %s\n", NULL, (char *)NULL);
-	printf("Test: %p, %s\n", NULL, (char *)NULL); 
+	ft_printf("Test: %p, %s\n", NULL, (char *) NULL);
+	printf("Test: %p, %s\n", NULL, (char *) NULL);
 	return (0);
 }
